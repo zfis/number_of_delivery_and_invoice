2aa0781b31a0f70d1beaee11b24bd218a18cfe99
ZeroSystems <sales@erpzero.com>
2019-12-29 04:46:44 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,30 @@
 # -*- coding: utf-8 -*-
-
+#############################################################################
+#
+#              Zero For Information Systems.
+#
+#  Copyright (C) 2019-TODAY Zero Systems(<https://www.erpzero.com>).
+#
+#  Author:Zero Systems(<https://www.erpzero.com>).
+#
+# All Rights Reserved.
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#############################################################################
 {
     'name': 'Number Of Delivery and_invoice',
     'version': '12.0.1.0',
     'summary': 'Number Of Delivery and_invoice',
-    'author': 'The256',
-    'company': 'The256',
+    'author': 'Zero Systems,cybrosys,sitaramsolutions',
+    'website': "https://www.erpzero.com",
+    'company': 'Zero For Information Systems',
     'depends': ['stock','account'],
     'data': [],
 
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
-    'application': True,
+    'application': False,
 }
